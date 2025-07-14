a1331ca75db4f75c3a2a4a6d1937a0bf1eecb666
Paul Morris <paulh.morris@gmail.com>
2025-07-14 14:43:22 -0500

    Relative import httpLogger

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { Geo, ipAddress } from "@vercel/functions";
 import { isbot } from "isbot";
 
-import { httpLogger } from "~/integrations/logger.server";
+import { httpLogger } from "./app/integrations/logger.server";
 
 export const config = { runtime: "nodejs" };
 
