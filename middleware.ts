b89d2c6a08f6e4f1e35987b3fbabef2eeed27641
Paul Morris <paulh.morris@gmail.com>
2025-07-15 21:00:24 -0500

    Minimal logging test

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,36 @@
 import { Axiom } from "@axiomhq/js";
 import { AxiomJSTransport, ConsoleTransport, Logger } from "@axiomhq/logging";
-import { geolocation, ipAddress, next, waitUntil } from "@vercel/functions";
-import { isbot } from "isbot";
+import { next, waitUntil } from "@vercel/functions";
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["/((?!assets|favicon.ico|site.webmanifest|.well-known).*)"],
+  matcher: ["/((?!assets|favicon|site.webmanifest|.well-known).*)"],
 };
 
-export default async function middleware(request: Request) {
-  const axiom = new Axiom({ token: process.env.AXIOM_TOKEN });
-  const datasets = await axiom.datasets.list();
-  console.log("Available datasets:", datasets);
-  const logger = new Logger({
-    transports: [new AxiomJSTransport({ axiom, dataset: "requests" }), new ConsoleTransport()],
-  });
+const axiom = new Axiom({ token: process.env.AXIOM_TOKEN });
+const logger = new Logger({
+  transports: [new AxiomJSTransport({ axiom, dataset: "requests" }), new ConsoleTransport()],
+});
 
-  const reqIsFromBot = request.headers.get("cf-isbot") === "true" || isbot(request.headers.get("user-agent") ?? "");
-  const geo = geolocation(request);
+export default function middleware(request: Request) {
+  // const reqIsFromBot = request.headers.get("cf-isbot") === "true" || isbot(request.headers.get("user-agent") ?? "");
+  // const geo = geolocation(request);
 
-  const logData: Record<string, unknown> = {
-    content_type: request.headers.get("content-type"),
-    geo: {
-      city: geo.city ?? request.headers.get("cf-ipcity"),
-      country: geo.country ?? request.headers.get("cf-ipcountry"),
-    },
-    id: request.headers.get("x-request-id"),
-    ip: ipAddress(request),
-    isbot: reqIsFromBot,
-    method: request.method,
-    uri: request.url,
-    user_agent: request.headers.get("user-agent"),
-  };
+  // const logData: Record<string, unknown> = {
+  //   content_type: request.headers.get("content-type"),
+  //   geo: {
+  //     city: geo.city ?? request.headers.get("cf-ipcity"),
+  //     country: geo.country ?? request.headers.get("cf-ipcountry"),
+  //   },
+  //   id: request.headers.get("x-request-id"),
+  //   ip: ipAddress(request),
+  //   isbot: reqIsFromBot,
+  //   method: request.method,
+  //   uri: request.url,
+  //   user_agent: request.headers.get("user-agent"),
+  // };
 
-  logger.info("http", logData);
+  logger.info("http", { test: true });
   waitUntil(logger.flush());
   return next();
 }
