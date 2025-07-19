112ffb467f160a9e213a5d9b67bf536a7fa7783b
Paul Morris <paulh.morris@gmail.com>
2025-07-18 22:35:18 -0500

    Trying no middleware

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,11 +1,8 @@
-import { requestId } from "hono/request-id";
 import { createHonoServer } from "react-router-hono-server/node";
 
-import { loggerMiddleware } from "~/server/middleware";
-
 export default await createHonoServer({
-  configure(server) {
-    server.use("*", requestId());
-    server.use("*", loggerMiddleware());
-  },
+  // configure(server) {
+  //   server.use("*", requestId());
+  //   server.use("*", loggerMiddleware());
+  // },
 });
