2ae899ec6d86d90033858f8a9b94934b16b28469
Paul Morris <paulh.morris@gmail.com>
2025-07-19 14:56:40 -0500

    Bump all minor packages, log server starting

--- app/server/index.ts
+++ app/server/index.ts
@@ -3,6 +3,8 @@ import { createHonoServer } from "react-router-hono-server/node";
 
 import { loggerMiddleware } from "~/server/middleware";
 
+console.log("Server starting...");
+
 export default await createHonoServer({
   configure(server) {
     server.use("*", requestId());
