0d0fa9f6d9a1a8457062129f40d3dc76f8ee23bb
Paul Morris <paulh.morris@gmail.com>
2025-07-19 22:21:44 -0500

    Try adding vercel handle to server

--- app/server.ts
+++ app/server.ts
@@ -1,11 +1,14 @@
 import { requestId } from "hono/request-id";
+import { handle } from "hono/vercel";
 import { createHonoServer } from "react-router-hono-server/node";
 
 import { loggerMiddleware } from "~/server/middleware";
 
-export default await createHonoServer({
-  configure(server) {
-    server.use("*", requestId());
-    server.use("*", loggerMiddleware());
-  },
-});
+export default handle(
+  await createHonoServer({
+    configure(server) {
+      server.use("*", requestId());
+      server.use("*", loggerMiddleware());
+    },
+  }),
+);
