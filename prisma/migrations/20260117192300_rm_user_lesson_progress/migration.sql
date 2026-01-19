c7d0f4fcc9535fd9582f1fdb5ead30b1cc903d1e
Paul Morris <paulh.morris@gmail.com>
2026-01-19 13:30:24 -0600

    Backfill userId with clerkId in migrations

--- prisma/migrations/20260117192300_rm_user_lesson_progress/migration.sql
+++ prisma/migrations/20260117192300_rm_user_lesson_progress/migration.sql
@@ -1,2 +1,8 @@
 -- DropForeignKey
 ALTER TABLE "UserLessonProgress" DROP CONSTRAINT "UserLessonProgress_userId_fkey";
+
+-- Backfill userId with clerkId from User
+UPDATE "UserLessonProgress"
+SET "userId" = "User"."clerkId"
+FROM "User"
+WHERE "UserLessonProgress"."userId" = "User"."id";
