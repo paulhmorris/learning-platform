e4e841ce766876168e651efc5adb61c1942a27fe
Paul Morris <paulh.morris@gmail.com>
2026-03-23 06:54:04 -0500

    Update support email

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,5 @@
 export const CONFIG = {
-  supportEmail: "help@plumblearning.com",
-  supportPhoneNumberRaw: "+14322668424",
-  supportPhoneNumberFormatted: "(432) 266-8424",
+  supportEmail: "support@plumblearning.com",
 };
 
 export const UserRole = {
