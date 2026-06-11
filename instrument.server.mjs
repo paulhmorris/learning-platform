b09279f9a61d8fcb9146f33dc8ac55849a8974b4
Paul Morris <paulh.morris@gmail.com>
2026-06-10 21:48:02 -0500

    Raise Sentry sampling to 1.0

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -2,7 +2,7 @@
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import * as Sentry from "@sentry/react-router";
 
-const isProd = process.env.VERCEL_ENV === "production";
+// const isProd = process.env.VERCEL_ENV === "production";
 
 Sentry.init({
   dsn: "https://3093e529a633d80d697b26390e53886d@o4505496663359488.ingest.us.sentry.io/4506584484151296",
@@ -11,8 +11,8 @@ Sentry.init({
 
   sampleRate: 1.0,
 
-  tracesSampleRate: isProd ? 0.5 : 1.0,
-  profileSessionSampleRate: isProd ? 0.5 : 1.0,
+  tracesSampleRate: 1.0,
+  profileSessionSampleRate: 1.0,
   profileLifecycle: "trace",
 
   sendDefaultPii: true,
