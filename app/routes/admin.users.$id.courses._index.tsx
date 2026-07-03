f82e2c43cdeee3a7397f92834c971f8a00fcd766
Paul Morris <paulh.morris@gmail.com>
2026-07-02 21:57:46 -0500

    Change to "view certificate"

--- app/routes/admin.users.$id.courses._index.tsx
+++ app/routes/admin.users.$id.courses._index.tsx
@@ -93,7 +93,7 @@ export default function AdminUserCourses() {
                         className="flex items-center gap-2"
                       >
                         <IconCertificate className="size-4" />
-                        <span>Download Certificate</span>
+                        <span>View Certificate</span>
                       </a>
                     </AdminButton>
                   ) : null}
